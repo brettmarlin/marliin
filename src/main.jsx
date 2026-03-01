c6e23f1f15afcf6b62fd82af5d465e2ce9b9cf43
Lumon <lumon@Lumons-Mac-mini.local>
2026-03-01 10:23:07 -0800

    Add /roll-ups landing page

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import Solutions from './Solutions.jsx'
+import RollUps from './RollUps.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/solutions" element={<Solutions />} />
+        <Route path="/roll-ups" element={<RollUps />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
